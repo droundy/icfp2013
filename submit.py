8dcce5ab86dab33b7d555b054ccd94a98a79fdb0
David Roundy <roundyd@physics.oregonstate.edu>
2013-08-11 16:54:00 -0700

    make submit.py crazy

--- submit.py
+++ submit.py
@@ -4,30 +4,15 @@ import sys
 import time
 
 def main():
-    if "get" in sys.argv:
-        n = int(sys.argv[2])
-        size = sys.argv[3]
-        i = 0
-        while i<n:
-            os.system("./get_training %s"%(size))
-            i+=1
-    if "solve" in sys.argv:
-        size = sys.argv[2]
-        if "problems" in sys.argv:
-            problems_str = "problem"
-            dirname = 'problems/%s' % (size)
-            probIDs = [name for name in os.listdir(dirname)]
-        else:
-            problems_str = ""
-            dirname = 'trainings/%s' % (size)
-            probIDs = [name for name in os.listdir(dirname)]
+        size = sys.argv[1]
+        problems_str = "problem"
+        dirname = 'problems/%s' % (size)
+        probIDs = [name for name in os.listdir(dirname)]
         for problem in probIDs:
             if os.path.exists(os.path.join(dirname, problem, "solved")):
                 print "problem %s is already solved" % problem
-            elif not os.path.exists(os.path.join(dirname, problem, "fast")):
-                print "problem %s is not fast" % problem
             else:
-                os.system("./make_guess %s %s %s fast"%(size, problem, problems_str))
+                os.system("./submit_parallel %s %s %s program master"%(size, problem, problems_str))
                 #time.sleep(10)
 
 if __name__ == '__main__':
