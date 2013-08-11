e3bdedc5febef5cb021a28249b382756bca8aed0
Paho Lurie-Gregg <paho@paholg.com>
2013-08-11 16:01:13 -0700

    added build to parallel.py

--- parallel.py
+++ parallel.py
@@ -8,6 +8,7 @@ import time
 size = sys.argv[1]
 dirname = 'problems/%s' % (size)
 probIDs = [name for name in os.listdir(dirname)]
+os.system("./build-all.sh")
 for problem in probIDs:
   if os.path.exists(os.path.join(dirname, problem, "solved")):
     print "problem %s is already solved" % problem
