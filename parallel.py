b81ca2819d0964cbf42a224765fe0c4c6664d8b4
Paho Lurie-Gregg <paho@paholg.com>
2013-08-11 15:59:22 -0700

    added parallel.py, new better way to submist batch jobs.

---
+++ parallel.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os
+import sys
+import time
+
+#
+
+size = sys.argv[1]
+dirname = 'problems/%s' % (size)
+probIDs = [name for name in os.listdir(dirname)]
+for problem in probIDs:
+  if os.path.exists(os.path.join(dirname, problem, "solved")):
+    print "problem %s is already solved" % problem
+  else:
+    os.system("sbatch -c4 batch_submit.sh %s %s" %(size, problem))
+    #time.sleep(10)
+
+
